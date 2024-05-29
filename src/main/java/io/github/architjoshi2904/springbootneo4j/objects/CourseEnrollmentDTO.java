c6b4a8a0fd63c392d0eed3e547497a4b2be5df2e
architJoshi2904 <aj6082@rit.edu>
2024-05-29 18:04:44 -0400

    Getters and setters generated

--- src/main/java/io/github/architjoshi2904/springbootneo4j/objects/CourseEnrollmentDTO.java
+++ src/main/java/io/github/architjoshi2904/springbootneo4j/objects/CourseEnrollmentDTO.java
@@ -20,6 +20,27 @@ public class CourseEnrollmentDTO {
         this.course = course;
     }
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername( String username ) {
+        this.username = username;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName( String name ) {
+        this.name = name;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse( Course course ) {
+        this.course = course;
+    }
 }
